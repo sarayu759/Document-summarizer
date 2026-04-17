3d477844220008a5d5b599c1069be2a79bb60cf4
sarayu759 <thangellasarayu.com>
2026-04-17 15:32:49 +0530

    final stable version

--- utils/gemini_vision.py
+++ utils/gemini_vision.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 def analyze_image(image_path):
