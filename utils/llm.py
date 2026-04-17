76e85365cc7e4fd58e202b120d60d1f384c9fb34
sarayu759 <thangellasarayu.com>
2026-04-17 15:50:19 +0530

    fix deployment

--- utils/llm.py
+++ utils/llm.py
@@ -1,12 +1,36 @@
 import streamlit as st
-from groq import Groq
+from openai import OpenAI
 
-client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
 
 def call_llm(prompt, max_tokens=300):
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="gpt-4o-mini",
         messages=[{"role": "user", "content": prompt}],
         max_tokens=max_tokens
     )
-    return response.choices[0].message.content.strip()
+    return response.choices[0].message.content
+
+
+def call_image_llm(base64_image):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Describe and summarize this image."},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/png;base64,{base64_image}"
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens=500,
+    )
+
+    return response.choices[0].message.content
