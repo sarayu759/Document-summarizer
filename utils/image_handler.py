fb77fa03da10757798d1604b930d546b5a6823cb
sarayu759 <thangellasarayu.com>
2026-04-22 19:42:02 +0530

    final faster version

--- utils/image_handler.py
+++ utils/image_handler.py
@@ -2,14 +2,19 @@ import pytesseract
 from PIL import Image
 import shutil
 
-# auto detect tesseract (works on Render + local)
 pytesseract.pytesseract.tesseract_cmd = shutil.which("tesseract") or "tesseract"
-
 
 def extract_image_text(path):
     try:
         img = Image.open(path)
+
+        # 🔥 resize for faster OCR
+        img = img.resize((800, 800))
+
+        # convert to grayscale
         img = img.convert("L")
+
         return pytesseract.image_to_string(img)
+
     except Exception as e:
         return f"OCR Error: {str(e)}"
