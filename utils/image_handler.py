a0e44e68ab9fe1a427652c9853083645916ad338
sarayu759 <thangellasarayu.com>
2026-04-22 19:09:57 +0530

    fix OCR for render

--- utils/image_handler.py
+++ utils/image_handler.py
@@ -1,21 +1,15 @@
 import pytesseract
 from PIL import Image
+import shutil
 
-# IMPORTANT
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+# auto detect tesseract (works on Render + local)
+pytesseract.pytesseract.tesseract_cmd = shutil.which("tesseract") or "tesseract"
 
 
 def extract_image_text(path):
     try:
         img = Image.open(path)
-
-        # improve OCR
         img = img.convert("L")
-        img = img.resize((img.width * 2, img.height * 2))
-
-        text = pytesseract.image_to_string(img, config='--psm 6')
-
-        return text
-
+        return pytesseract.image_to_string(img)
     except Exception as e:
         return f"OCR Error: {str(e)}"
